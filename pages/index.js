c0af159d3ff148e24bfa4ffbc156a8565dc7fa73
Balwant <select2007@gmail.com>
2021-02-25 18:57:00 +0530

    fresh upload

--- pages/index.js
+++ pages/index.js
@@ -17,56 +17,6 @@ export default function Home() {
     </div>
   </div>
 
-<div className="max-w-5xl mx-auto px-4 sm:px-6 py-8">
-
-  <div className="font-medium leading-7 text-center space-y-2 sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 sm:space-y-0">
-
-    <div className="text-teal-900 bg-teal-200 py-3 px-6 rounded">
-      <p>Hello, you! 👋</p>
-    </div>
-
-    <div className="text-purple-900 bg-purple-200 py-3 px-6 rounded">
-      <p>Here is a 🎁</p>
-    </div>
-
-    <div className="text-green-900 bg-green-200 py-3 px-6 rounded">
-      <p>Tailwind starter 🎉</p>
-    </div>
-  </div>
-</div>
-
-
-    <div className="mt-6">
-
-      <button className="px-4 py-2 text-white bg-blue-500 rounded select-none no-outline focus:shadow-outline">Open Modal</button>
-
-      <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-
-        
-           <div className="h-auto p-4 mx-2 text-left bg-white rounded shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0">
-          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Modal Title
-            </h3>
-
-            <div className="mt-2">
-              <p className="text-sm leading-5 text-gray-500">
-                Adipisci quasi doloribus. Veniam veritatis dignissimos. Quis maiores ea. Et nulla sunt.
-              </p>
-          </div>
-        </div>
-
-          <div className="mt-5 sm:mt-6">
-            <span className="flex w-full rounded-md shadow-sm">
-              <button className="inline-flex justify-center w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700">
-                Close this modal!
-              </button>
-            </span>
-          </div>
-
-        </div>
-      </div>
-    </div>
     </>
   )
 }
