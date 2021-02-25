e042978bb84fdc9d9198c2384e509c0014f6d7d1
Balwant <select2007@gmail.com>
2021-02-25 18:59:15 +0530

    fresh upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Hero from '../components/Hero'
+//import Hero from '../components/Hero'
 export default function Layout({children}) {
   return (
     <>
